8b6c24ccc67efa1d2614627008b981aaf7efc64f
Hall1110 <160109460+Hall1110@users.noreply.github.com>
2024-08-06 12:33:34 +0200

    Første upload
    Opgaver i forbindelse med Hoverforløb 1

---
+++ Basic/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(AddAndMultiply(2, 4, 5));
+            //Console.WriteLine(CelciusToFahrenheit(0));
+            //Console.WriteLine(CelciusToFahrenheit(100));
+            //Console.WriteLine(CelciusToFahrenheit(-300));
+            //ElementaryOperations(3, 8);
+            //Console.WriteLine(IsResultTheSame(2 + 2, 2 * 2));
+            //Console.WriteLine(IsResultTheSame(9 / 3, 16 - 1));
+            //Console.WriteLine(ModuloOperations(8, 5, 2));
+            //Console.WriteLine(CubeOf(2));
+            //Console.WriteLine(CubeOf(-5.5));
+            //SwapTwoNumbers(87, 45);
+            //SwapTwoNumbers(-13, 2);
+        }
+ 
+        
+        
+        static int AddAndMultiply(int a, int b, int c)
+        {
+            return (a + b) * c;
+        }
+
+
+
+        static string CelciusToFahrenheit(double celcius)
+        {
+            double fahrenheit = celcius * 1.8 + 32;
+            string message;
+            if (fahrenheit < -459)
+            {
+                message = "Temperature below absolute zero!";
+                return message;
+            }
+            else
+            {
+                message = "C = " + fahrenheit + "F";
+                return message;
+            }
+        }
+
+
+
+        static void ElementaryOperations(int a, int b)
+        {
+            int addition = a + b;
+            int subtraction = a - b;
+            int multiplication = a * b;
+            int division;
+            if (a == 0 || b == 0)
+            {
+                Console.WriteLine("Divide by 0 Error");
+                division = 0;
+            }
+            else
+            {
+                division = a / b;
+            }
+            int[] operations = [addition, subtraction, multiplication, division];
+            for (int i = 0; i < operations.Length; i++)
+            {
+                Console.WriteLine(operations[i]);
+            }
+        }
+
+
+
+        static bool IsResultTheSame(int a, int b)
+        {
+            return (a == b);
+        }
+
+
+
+        static int ModuloOperations(int a, int b, int c)
+        {
+            int result = a % b % c;
+            return result;
+        }
+
+
+
+        static double CubeOf(double a)
+        {
+            return a * a * a;
+        }
+
+
+
+        static void SwapTwoNumbers(int a, int b)
+        {
+            Console.WriteLine("Before: a = " + a + ", b = " + b);
+            int c = b;
+            b = a;
+            a = c;
+            Console.WriteLine("After: a = " + a + ", b = " + b);
+        }
+    }
+}
---
+++ Conditional Statements/Program.cs
@@ -0,0 +1,160 @@
+﻿namespace ConditionalStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(AbsoluteValue(6832));
+            //Console.WriteLine(AbsoluteValue(-392));
+            //Console.WriteLine(DivisibleBy(15, 30));
+            //Console.WriteLine(DivisibleBy(2, 90));
+            //Console.WriteLine(DivisibleBy(7, 12));
+            //Console.WriteLine(IsUppercase("xyz"));
+            //Console.WriteLine(IsUppercase("DOG"));
+            //Console.WriteLine(IsUppercase("l9#"));
+            //Console.WriteLine(IfGreaterThanThird([2, 7, 12]));
+            //Console.WriteLine(IfGreaterThanThird([-5, -8, 50]));
+            //Console.WriteLine(IsNumberEven(721));
+            //Console.WriteLine(IsNumberEven(1248));
+            //Console.WriteLine(IfSortedAscending([3, 7, 10]));
+            //Console.WriteLine(IfSortedAscending([74, 62, 99]));
+            //Console.WriteLine(PositiveNegativeOrZero(5.24));
+            //Console.WriteLine(PositiveNegativeOrZero(0.0));
+            //Console.WriteLine(PositiveNegativeOrZero(-994.53));
+            //Console.WriteLine(IfLeapYear(2016));
+            //Console.WriteLine(IfLeapYear(2018));
+            //Console.WriteLine(IfLeapYear(2000));
+        }
+
+
+
+        static int AbsoluteValue(int a)
+        {
+            if (a < 0)
+            {
+                return a * -1;
+            }
+            else
+            {
+                return a;
+            }
+        }
+    
+
+
+        static decimal DivisibleBy(decimal a, decimal b)
+        {
+            if (a % 2 == 0 && b % 2 == 0)
+            {
+                return a * b;
+            }
+            else if (a % 3 == 0 && b % 3 == 0)
+            {
+                return a * b;
+            }
+            else
+            {
+                return a + b;
+            }
+        }
+
+
+
+        static bool IsUppercase(string a)
+        {
+            
+            foreach (char b in a)
+            {
+                if (!Char.IsUpper(b))
+                {
+                    return false;
+                }
+
+            }
+                return true;
+        }
+
+
+
+        static bool IfGreaterThanThird(int[] a)
+        {
+            int multiplication = a[0] * a[1];
+            int addition = a[0] + a[1];
+            if (multiplication > a[2] || addition > a[2])
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+
+
+        static bool IsNumberEven(int a)
+        {
+            if (a % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        static bool IfSortedAscending(int[] a)
+        {
+            int arrayLength = a.Length;
+            for (int i = 1; i < arrayLength; i++)
+            {
+                if (a[i] < a[i - 1])
+                {
+                    return false;
+                }
+            }
+                 return true;
+        }
+
+
+
+        static string PositiveNegativeOrZero(double a)
+        {
+            if (a < 0)
+            {
+                return "Negative";
+            }
+            else if (a == 0)
+            {
+                return "Zero";
+            }
+            else
+            {
+                return "Positive";
+            }
+        }
+
+
+
+        static bool IfLeapYear(int a)
+        {
+            
+            if (a % 4 == 0)
+            {
+                if (a % 4 == 0 && a % 100 == 0)
+                {
+                    if(a % 400 == 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
